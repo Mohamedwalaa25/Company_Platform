d63a79192e2eed29f4de27623fed7b993ee4b559
Mohamedwalaa25 <mohamedwalaa250@gmail.com>
2024-03-02 19:58:35 +0200

    enhancing Operations

--- resources/views/admin/settings/update-settings.blade.php
+++ resources/views/admin/settings/update-settings.blade.php
@@ -111,14 +111,7 @@
     </div>
         <div class="mb-12 mt-4">
             <button class="btn btn-primary" type="submit" wire:loading.attr='disabled'>
-            <span wire:loading.remove>
-               Submit
-            </span>
-                <div class="text-center" wire:loading wire:target='submit'>
-                <span class="spinner-border spinner-border-sm text-white" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </span>
-                </div>
+                @include('admin.loadin',['btnName'=>'Submit'])
             </button>
         </div>
 </form>
--- resources/views/admin/skills/skills-create.blade.php
+++ resources/views/admin/skills/skills-create.blade.php
@@ -1,41 +1,17 @@
-<div class="modal fade" id="basicModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel1">Create New Skill</h5>
-                <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                ></button>
-            </div>
-            <form wire:submit.prevent="submit">
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col mb-0">
-                        <label class="form-label">Name</label>
-                        <input type="text"  class="form-control" placeholder="Name" wire:model="name" />
-                        @error('name')
-                        <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="col mb-0">
-                        <label  class="form-label">Progress</label>
-                        <input type="number"  class="form-control" placeholder="Progress" min="1" max="100" wire:model="progress"/>
-                        @error('progress')
-                        <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                    Close
-                </button>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-            </form>
-        </div>
+<x-create-model title="Add New Skill">
+    <div class="col mb-0">
+        <label class="form-label">Name</label>
+        <input type="text"  class="form-control" placeholder="Name" wire:model="name" />
+        @error('name')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
     </div>
-</div>
+    <div class="col mb-0">
+        <label  class="form-label">Progress</label>
+        <input type="number"  class="form-control" placeholder="Progress" min="1" max="100" wire:model="progress"/>
+        @error('progress')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+</x-create-model>
+
